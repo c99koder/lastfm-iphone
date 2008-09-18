c13462d72a90025277bfdb5c16d2fb3748fc2b09
Sam Steele <sam@last.fm>
2008-09-18 23:21:15 +0000

    Slap the GPL header onto DebugViewController.[m|h]

--- Classes/DebugViewController.h
+++ Classes/DebugViewController.h
@@ -1,13 +1,23 @@
-//
-//  DebugViewController.h
-//  MobileLastFM
-//
-//  Created by Sam Steele on 9/17/08.
-//  Copyright 2008 Last.fm. All rights reserved.
-//
+/* DebugViewController.h - Displays debug statistics
+ * Copyright (C) 2008 Sam Steele
+ *
+ * This file is part of MobileLastFM.
+ *
+ * MobileLastFM is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
+ *
+ * MobileLastFM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ */
 
 #import <UIKit/UIKit.h>
-
 
 @interface DebugViewController : UIViewController {
 	IBOutlet UILabel *_httpBuffers;
