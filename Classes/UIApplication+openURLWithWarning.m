87fd7144a28fb3138c77eb3aa0ab7964eb642b23
Sam Steele <sam@last.fm>
2009-06-04 16:30:22 +0000

    Updated to GPLv3

--- Classes/UIApplication+openURLWithWarning.m
+++ Classes/UIApplication+openURLWithWarning.m
@@ -5,9 +5,10 @@
  *
  * This file is part of MobileLastFM.
  *
- * MobileLastFM is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2
- * as published by the Free Software Foundation.
+ * MobileLastFM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
  * MobileLastFM is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -15,8 +16,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ * along with MobileLastFM.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #import "UIApplication+openURLWithWarning.h"
