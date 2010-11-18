b5fe17f7840c1f07e6ee5c5f0f0ae387161ed2dd
Sam Steele <sam@last.fm>
2010-11-18 09:31:13 -0800

    Increase recent tracks to 10

--- Classes/ProfileViewController.m
+++ Classes/ProfileViewController.m
@@ -77,7 +77,7 @@
 	
 	if([_recentTracks count]) {
 		stations = [[NSMutableArray alloc] init];
-		for(int x=0; x<[_recentTracks count] && x < 5; x++) {
+		for(int x=0; x<[_recentTracks count] && x < 10; x++) {
 			[stations addObject:[NSDictionary dictionaryWithObjects:[NSArray arrayWithObjects:[[_recentTracks objectAtIndex:x] objectForKey:@"name"], [[_recentTracks objectAtIndex:x] objectForKey:@"artist"], [[_recentTracks objectAtIndex:x] objectForKey:@"image"],
 																															 [NSString stringWithFormat:@"lastfm-track://%@/_/%@", [[[_recentTracks objectAtIndex:x] objectForKey:@"artist"] URLEscaped], [[[_recentTracks objectAtIndex:x] objectForKey:@"name"] URLEscaped]],nil] forKeys:[NSArray arrayWithObjects:@"title", @"artist", @"image", @"url",nil]]];
 		}
