fd31395d8df485c29d825957242ea98812aa9dc3
Sam Steele <sam@last.fm>
2011-02-04 12:59:27 -0800

    Add GPL headers to ShareActionSheet and UIColor+LastFMColors

--- Classes/ShareActionSheet.h
+++ Classes/ShareActionSheet.h
@@ -1,10 +1,23 @@
-//
-//  ShareActionSheet.h
-//  MobileLastFM
-//
-//  Created by Jono Cole on 04/02/2011.
-//  Copyright 2011 Last.fm. All rights reserved.
-//
+/* ShareActionSheet.h - Display an share action sheet
+ * 
+ * Copyright 2011 Last.fm Ltd.
+ *   - Primarily authored by Sam Steele <sam@last.fm>
+ *
+ * This file is part of MobileLastFM.
+ *
+ * MobileLastFM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MobileLastFM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MobileLastFM.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #import <UIKit/UIKit.h>
 #import <MessageUI/MessageUI.h>
